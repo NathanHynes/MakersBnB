4db9d32dd34f5d8c4a92d93b45424f36d45cf608
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-22 10:25:53 +0100

    User table created - with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
+require 'bcrypt'
+
 class User 
   include DataMapper::Resource
 
+  attr_accessor :password
+
   property :id, Serial
-  property :forename, String, :length => 40, :required => true
+  property :forename, String, :length => 40, :required => true,
   :messages => {
       :presence => "Please provide a first name",
     }
-  property :surname, String, :length => 40, required => true
+  property :surname, String, :length => 40, :required => true,
   :messages => {
       :presence => "Please provide a surname name",
     }
-  property :email, String, :required => true, :unique => true, :format => :email_address
+  property :email, String, :required => true, :unique => true, :format => :email_address,
     :messages => {
       :presence => "Please provide e-mail address",
       :is_unique => "E-mail address already used",
@@ -21,4 +25,13 @@ class User
     :presence => "Please provide username",
     :is_unique => "Username already used",
   }
+  property :crypted_pass, String, :length => 60..60, :required => true, :writer => :protected
+
+  has n, :listings
+
+  def password=(password)
+    @password = password
+    self.crypted_pass = BCrypt::Password.create(password)
+  end
+
 end 
