885ca371898b3d604ffddd74dcd745707d2fb31d
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-23 13:57:47 +0100

    nice calamnder form

---
+++ app/public/src/interface.js
@@ -0,0 +1,11 @@
+
+  $(function () {
+    $('#datetimepicker1').datetimepicker({minDate:'2019/12/03', maxDate:'2019/12/24'});
+ });
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"></link>
