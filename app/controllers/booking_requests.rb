5e054a44e125e97055c76c1124b6edf6071d545f
Student <student@Makerss-MacBook-Pro.local>
2019-10-23 10:52:42 +0100

    request booking saves to table

--- app/controllers/booking_requests.rb
+++ app/controllers/booking_requests.rb
@@ -3,9 +3,15 @@ class MakersBnB < Sinatra::Base
   get '/booking_requests/:id/new' do
     p Listing.get(params[:id])
     @listing = Listing.get(params[:id])
+    p User.get(@listing.user_id)
     erb :'booking_requests/new'
   end
 
-
+  post '/bookingrequests' do
+    booking = Bookingrequest.create(requested_date: params[:date], listing_id: params[:listing_id], user_id: current_user.id)
+    p booking.save
+    p booking
+    redirect '/listings'
+  end
 
 end
