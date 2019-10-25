4f5b74d335a19d65bce982c627f18b7ac9eb05d1
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-25 13:42:21 +0100

    images displayed per listing

--- app/controllers/listings.rb
+++ app/controllers/listings.rb
@@ -20,42 +20,13 @@ class MakersBnB < Sinatra::Base
       user_id: current_user.id,
       image: params[:image]
     )
-
-      @listing_image = listing.image
-
-      p "this file is: #{@listing_image}"
-
     img = Image.new
-    
 
     #Save the data from the request
      img.image = params[:image] #carrierwave will upload the file automatically
 
     #Save
      img.save
-
     redirect '/listings'
   end
-
-  # post "/test" do
-
-  #   #Create new Image Model
-  #   img = Image.new
-  #   p img
-
-  #   #Save the data from the request
-  #    img.image = params[:image] #carrierwave will upload the file automatically
-
-  #   #Save
-  #   p img.save
-  #   redirect '/test'
-
-  # end
-
-  # get '/test' do 
-  #   image = Image.last
-  #   p image
-  #   # erb :test
-  # end 
-
 end
